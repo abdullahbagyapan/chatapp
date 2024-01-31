647aa1809ee27dd094d189682cdbc183d914d8b3
Abdullah Bagyapan <abdullahbagyapan@hotmail.com>
2024-01-31 12:09:03 +0300

    Add Start() and NewServer() functions

--- server.go
+++ server.go
@@ -14,4 +14,31 @@ type Server struct {
 	ln         net.Listener
 	clientMap  map[*net.Addr]net.Conn
 	msgch      chan Message
+	quitch     chan struct{}
+}
+
+func NewServer() *Server {
+	return &Server{
+		listenAddr: "localhost:5000",
+		msgch:      make(chan Message, 10),
+		clientMap:  make(map[*net.Addr]net.Conn, 10),
+	}
+}
+
+func (s *Server) Start() error {
+
+	ln, err := net.Listen("tcp", s.listenAddr)
+
+	if err != nil {
+		return err
+	}
+
+	defer ln.Close()
+
+	s.ln = ln
+
+	<-s.quitch
+
+	return nil
+
 }
